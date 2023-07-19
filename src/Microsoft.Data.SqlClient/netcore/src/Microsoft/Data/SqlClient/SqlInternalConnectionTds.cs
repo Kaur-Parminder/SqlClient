139375845c7d185379c7b2ee33042806e1e33187
Parminder Kaur <v-parkaur@microsoft.com>
2023-07-19 14:04:00 -0700

    passing localdb as np as default servername

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SqlInternalConnectionTds.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/SqlClient/SqlInternalConnectionTds.cs
@@ -1913,12 +1913,19 @@ namespace Microsoft.Data.SqlClient
             SqlClientEventSource.Log.TryAdvancedTraceEvent("<sc.SqlInternalConnectionTds.AttemptOneLogin|ADV> {0}, timout={1}[msec], server={2}", ObjectID, timeout.MillisecondsRemaining, serverInfo.ExtendedServerName);
             RoutingInfo = null; // forget routing information 
             string localDBDataSource = null;
-
+            DataSource details = null;
             if (ConnectionOptions.LocalDBInstance != null)
             {
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
                     localDBDataSource = LocalDB.GetLocalDBDataSource(serverInfo.UserServerName, timeout);
+                    string serverName = localDBDataSource ?? serverInfo.ExtendedServerName;
+                    details = DataSource.ParseServerName(serverName);
+
+                    //re-writing serverinfo 
+                    ServerInfo original = serverInfo;
+                    serverInfo = new ServerInfo(ConnectionOptions, serverName, original.ServerSPN);
+                    serverInfo.SetDerivedNames(original.UserProtocol, serverName);
                 }
 #if NETCOREAPP || NETSTANDARD
                 else
@@ -1927,9 +1934,6 @@ namespace Microsoft.Data.SqlClient
                 }
 #endif
             }
-
-            string serverName = localDBDataSource ?? serverInfo.ExtendedServerName;
-            DataSource details = DataSource.ParseServerName(serverName);
 
             _parser._physicalStateObj.SniContext = SniContext.Snix_Connect;
 
